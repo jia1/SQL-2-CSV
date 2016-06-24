0bc7d3fee2e928fdeb78e6f909b433ded3167125
Jia Yee <jiayeerawr@gmail.com>
2016-06-24 10:44:21 +0800

    Escape special characters
    Comma, quotation marks, and others

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 
@@ -11,47 +12,104 @@ namespace SQLServerToCSV
 {
     class Program
     {
-        static string dbServer = "XXXX";
-        static string dbUser = "XXXX";
-        static string dbPwd = "XXXX";
-        static string connectionString = String.Format(
+        private static string dbServer = "";
+        private static string dbUser = "";
+        private static string dbPwd = "";
+        private static string connectionString = String.Format(
             "data source={0};initial catalog={1};User ID={2};Password={3}", 
-            dbServer, XXXX, dbUser, dbPwd);
-        static string queryString = "SELECT XXXX FROM XXXX WHERE XXXX AND XXXX";
-        static string winUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-        static string filePath = String.Format("C:\\Users\\{0}\\XXXX\\XXXX", winUser);
+            dbServer, , dbUser, dbPwd);
+        private static string tableName = "";
+        private static string queryString = String.Format("SELECT * FROM {0}", tableName);
+        private static string winUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        private static string filePath = String.Format("C:\\Users\\{0}\\Desktop\\{1}.csv", winUser, tableName);
 
         private static void SQLToCSV()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
-                command.Connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                try
+                // SqlCommand command = new SqlCommand(queryString, connection);
+                // command.Connection.Open();
+                // SqlDataReader reader = command.ExecuteReader();
+                connection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(
+                    queryString, connection))
                 {
-                    StreamWriter writer = new StreamWriter(filePath, false);
-                    for (int i = 0; i < reader.FieldCount - 1; i++)
-                        writer.Write(String.Format("{0},", reader.GetName(i)));
-                    writer.Write(reader.GetName(reader.FieldCount - 1));
-                    writer.Write(writer.NewLine);
-
-                    while (reader.Read())
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    DataTableReader reader = table.CreateDataReader();
+                    try
                     {
-                        for (int i = 0; i < reader.FieldCount - 1; i++)
-                            writer.Write(String.Format("{0},", reader.GetValue(i)));
-                        writer.Write(reader.GetValue(reader.FieldCount - 1));
-                        writer.Write(writer.NewLine);
-                    }
+                        StreamWriter writer = new StreamWriter(filePath, false);
 
-                    writer.Close();
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("IOException: Please close the file!");
-                    Console.ReadLine();
+                        for (int i = 0; i < reader.FieldCount - 1; i++)
+                            writer.Write(String.Format("{0},", reader.GetName(i)));
+                        writer.Write(reader.GetName(reader.FieldCount - 1));
+                        writer.Write(writer.NewLine);
+
+                        /*
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount - 1; i++)
+                                writer.Write(String.Format("{0},", reader.GetValue(i)));
+                            writer.Write(reader.GetValue(reader.FieldCount - 1));
+                            writer.Write(writer.NewLine);
+                        }
+                        */
+
+                        string colVal = string.Empty;
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount - 1; i++)
+                            {
+                                colVal = String.Format("{0}", reader.GetValue(i));
+                                colVal = StringToCSVCell(colVal);
+
+                                /*
+                                if (colVal.Contains(','))
+                                {
+                                    colVal = colVal.Replace('\"', '\'');
+                                    colVal = String.Format("\"{0}\"", colVal);
+                                }
+                                */ 
+
+                                writer.Write(String.Format("{0},", colVal));
+                            }
+                            writer.Write(reader.GetValue(reader.FieldCount - 1));
+                            writer.Write(writer.NewLine);
+                        }
+
+                        writer.Close();
+                        Console.WriteLine("Execution Successful. Press Enter to Exit.");
+                        Console.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine("IOException: Please close the file!");
+                        Console.WriteLine("Press Enter to Exit.");
+                        Console.ReadLine();
+                    }
                 }
             }
+        }
+
+        public static string StringToCSVCell(string value)
+        {
+            bool quote = (value.Contains(",") || value.Contains("\"") || 
+                value.Contains("\r") || value.Contains("\n"));
+            if (quote)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append("\"");
+                foreach (char nextChar in value)
+                {
+                    builder.Append(nextChar);
+                    if (nextChar == '"')
+                        builder.Append("\"");
+                }
+                builder.Append("\"");
+                return builder.ToString();
+            }
+            return value;
         }
 
         static void Main(string[] args)
