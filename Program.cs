baaf62a1835620fb017bed105d1a75dc3965dddf
Jia Yee <jiayeerawr@gmail.com>
2016-06-24 11:13:24 +0800

    Enhance exception handling
    Catches UnauthorizedAccessException and also improve on all other status messages

--- Program.cs
+++ Program.cs
@@ -22,7 +22,8 @@ namespace SQLServerToCSV
         private static string queryString = String.Format("SELECT * FROM {0}", tableName);
         private static string winUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
         private static string filePath = String.Format("C:\\Users\\{0}\\Desktop\\{1}.csv", winUser, tableName);
-
+        private static bool appendOption = false;
+        
         private static void SQLToCSV()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -36,7 +37,7 @@ namespace SQLServerToCSV
                     DataTableReader reader = table.CreateDataReader();
                     try
                     {
-                        StreamWriter writer = new StreamWriter(filePath, false);
+                        StreamWriter writer = new StreamWriter(filePath, appendOption);
 
                         for (int i = 0; i < reader.FieldCount - 1; i++)
                             writer.Write(String.Format("{0},", reader.GetName(i)));
@@ -57,12 +58,36 @@ namespace SQLServerToCSV
                         }
 
                         writer.Close();
-                        Console.WriteLine("Execution Successful. Press Enter to Exit.");
+                        Console.WriteLine("Execution Successful!");
+                        Console.WriteLine(String.Format("The file '{0}' was successfully", filePath));
+                        Console.WriteLine("created or overwritten.");
+                        Console.WriteLine();
+                        Console.WriteLine("Press Enter to Exit.");
                         Console.ReadLine();
                     }
                     catch (IOException)
                     {
-                        Console.WriteLine("IOException: Please close the file!");
+                        Console.WriteLine("IOException:");
+                        Console.WriteLine("The process cannot access the file");
+                        Console.WriteLine(String.Format("'{0}' ", filePath));
+                        Console.WriteLine("because it is being used by another process.");
+                        Console.WriteLine();
+                        Console.WriteLine("Please close the file before running this application.");
+                        Console.WriteLine();
+                        Console.WriteLine("Press Enter to Exit.");
+                        Console.ReadLine();
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine("UnauthorizedAccessException:");
+                        Console.WriteLine(String.Format("Access to the path '{0}' is denied.", filePath));
+                        Console.WriteLine();
+                        Console.WriteLine("Please check that the following are true:");
+                        Console.WriteLine("1. File is not ReadOnly, and");
+                        Console.WriteLine("2. You have sufficient privileges to access this file.");
+                        Console.WriteLine();
+                        Console.WriteLine("You may configure the application to write to another directory or file.");
+                        Console.WriteLine();
                         Console.WriteLine("Press Enter to Exit.");
                         Console.ReadLine();
                     }
